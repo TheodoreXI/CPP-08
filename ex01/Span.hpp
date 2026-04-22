6131c82e36192ee3e23fdc2442643d5eec095166
Abdelhadi Akroud <aakroud@e2r7p14.1337.ma>
2026-04-22 14:48:26 +0100

    maybe

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -1,8 +1,26 @@
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
+#include <iostream>
 
-
+class Span
+{
+    private:
+        std::vector<int> v;
+        size_t           c;
+    public:
+        Span(void);
+        ~Span(void);
+        Span &operator=(const Span &obj);
+        Span(const Span &obj);
+        Span(unsigned int N);
+        void addNumber(unsigned int N);
+        int shortestSpan(void);
+        int longestSpan(void);
+};
 
 
 
--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,5 +1,36 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <deque>
+
+
+template <typename T> 
+class MutantStack : public std::stack<T>
+{
+    typedef typename std::deque<T>::iterator iterator;
+    private:
+        typename std::deque<T>::iterator it;
+    public:
+        MutantStack(void)
+        {
+            std::cout << "default constructor called\n";
+        }
+        iterator begin(void)
+        {
+            it = this->c.begin();
+            return (it);
+        }
+        iterator end(void)
+        {
+            it = this->c.end();
+            return (it);
+        }
+
+
+};
+
 
 #endif
